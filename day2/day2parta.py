e1dae20d655486cb8347a39c51c7b67f3591118d
wwvmd <vm.codemonkey@yahoo.com>
2024-12-02 15:10:32 +0000

    wip

--- day2/day2parta.py
+++ day2/day2parta.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
+def compare(a, b, asc):
+    if asc is None:
+        asc = a < b
+
+        
+    return a < b == asc, asc
+
 # Read each line of input/test.txt into a pandas Series
 file_path = 'day2/input/test.txt'
 
@@ -8,9 +15,16 @@ with open(file_path, 'r') as file:
     print(lines)
 
     for lin in lines:
-
+        asc = None
         for i in range(len(lin)):
             print(lin[i])
+            next = i+1
+            if next > len(lin):
+                break
+            valid,asc =  compare(lin[i], lin[next],asc)
+
+
+
         print('end of lin')
 
 
