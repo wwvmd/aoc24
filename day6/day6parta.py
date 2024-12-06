2a86a8e58970d5eea9bfd80b7bad2de7f3fd1997
wwvmd <wwvmd@users.noreply.github.com>
2024-12-06 19:43:40 +0000

    Day 6 WIP

---
+++ day6/day6parta.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+if __name__ == "__main__":
+    file_path = 'day6/input/test.txt'
+    lines = []
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    df = None
+    facing = 'UP'
+    array_2d = [line.strip().split() for line in lines]
+    for row in array_2d:
+        print(row)
+        df = pd.DataFrame(array_2d)
+        print(df)
+        df = pd.DataFrame([list(line.strip()) for line in lines])
+    print(df)
+ 
+    print(df.iloc[6, 4])
+
+
+
+
+    def move(df,x, y):
+
+        #Before moving check if the next position
+
+
+        if df.iloc[x-1, y] == '#':
+            print('Cannot move to this position')
+            return False
+        
+
+        #When moving up you want stay in the same column but move up a row
+        #replace the current position with a  X
+        df.iloc[x, y] = 'X'
+        df.iloc[x - 1, y] = '^'
+
+
+    moveUp(df, 6, 4)
+    print(df)
